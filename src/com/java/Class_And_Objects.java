2e39f8ff42333eb3d8dec4aa2cc949ed8f556e14
Lashkare Parth Vijaykumar <57425927+thyfencercodes@users.noreply.github.com>
2023-03-29 14:29:11 +0530

    class and objects

---
+++ src/com/java/Class_And_Objects.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.java;
+
+/**
+ * @author parth
+ *
+ */
+public class Class_And_Objects {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
