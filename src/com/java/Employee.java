14bee78f1a7c7266ab201db2f4724e421d37bb90
Lashkare Parth Vijaykumar <57425927+thyfencercodes@users.noreply.github.com>
2023-03-30 16:59:28 +0530

    employee and main employee data

---
+++ src/com/java/Employee.java
@@ -0,0 +1,90 @@
+package com.test.app;
+
+public class Employee {
+	//Camelcase
+	//Data members/variables, properties, instance varables
+	private long employeeId;
+	private String employeeName;
+	private double employeeSalary;
+	private String employeeEmail;
+	private long employeePhone;
+	
+	
+	//default constructor
+	public Employee()
+	{
+		System.out.println("default constructor invoked");
+	}
+	
+	//parameterized constructor
+	public Employee(long employeeId,String employeeName,double employeeSalary, String employeeEmail, long employeePhone)
+	{
+		this.employeeId=employeeId;
+		this.employeeName=employeeName;
+		this.employeePhone=employeePhone;
+		this.employeeSalary=employeeSalary;
+		this.employeeEmail=employeeEmail;
+	}
+	
+	
+	
+	public long getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(long employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	public double getEmployeeSalary() {
+		return employeeSalary;
+	}
+
+	public void setEmployeeSalary(double employeeSalary) {
+		this.employeeSalary = employeeSalary;
+	}
+
+	public String getEmployeeEmail() {
+		return employeeEmail;
+	}
+
+	public void setEmployeeEmail(String employeeEmail) {
+		this.employeeEmail = employeeEmail;
+	}
+
+	public long getEmployeePhone() {
+		return employeePhone;
+	}
+
+	public void setEmployeePhone(long employeePhone) {
+		this.employeePhone = employeePhone;
+	}
+
+	public Employee(long employeeId, String employeeName, double employeeSalary) {
+		
+		this.employeeId = employeeId;
+		this.employeeName = employeeName;
+		this.employeeSalary = employeeSalary;
+	}
+
+	void salaryCheck()
+	{
+		System.out.println(this.employeeSalary*12);
+		
+	}
+	
+	void displayEmployeeDetails()
+	{
+		System.out.println(this.employeeId + " "+this.employeeName+" "+this.employeeSalary+" "+this.employeePhone+" "+this.employeeEmail);
+		
+	}
+	
+}
---
+++ src/com/java/MainEmployee.java
@@ -0,0 +1,27 @@
+package com.test.app;
+
+public class MainEmployee {
+
+	public static void main(String[] args) {
+		
+		Employee emp=new Employee();
+		emp.setEmployeeId(7580);
+		emp.setEmployeeName("Mr. Raman");
+		emp.setEmployeeEmail("raman@gmail.com");
+		emp.setEmployeePhone(93993838);
+		emp.setEmployeeSalary(70000);
+		
+		Employee emp1=new Employee(363637,"Mr. Kumar",50000,"kumar@gmail.com",83839393);
+		
+		emp.displayEmployeeDetails();
+		emp1.displayEmployeeDetails();
+		
+		
+		emp.salaryCheck();
+		emp1.salaryCheck();
+		
+		System.out.println(emp.getEmployeeId()+" "+emp.getEmployeeName()+" "+emp.getEmployeeSalary());
+	
+	}
+
+}
